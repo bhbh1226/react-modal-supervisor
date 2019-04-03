8a1031f5f8840da4812e05e66d26f96a99d06040
bhbh1226 <bhbh1226@naver.com>
2019-04-03 22:22:16 +0900

    custom style componentify

--- examples/App.js
+++ examples/App.js
@@ -7,29 +7,29 @@ import './normalize.css'
 
 modalRootInit()
 
+const CustomBackground = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    justify-content: center;
+    align-items: center;
+
+    background-color: rgba(12, 243, 123, 0.4);
+    position: fixed;
+
+    top: 0;
+    left: 0;
+
+    height: 100vh;
+    width: 100%;
+
+    z-index: 9000;
+`
+
 const NewCustomStyle = StyleManagerHOC(({styleOverride}) => {
-    styleOverride(STYLE_BACKGROUND, styled.div`
-        display: flex;
-        flex-direction: column;
+    styleOverride(STYLE_BACKGROUND, CustomBackground)
 
-        justify-content: center;
-        align-items: center;
-
-        background-color: rgba(12, 243, 123, 0.4);
-        position: fixed;
-
-        top: 0;
-        left: 0;
-
-        height: 100vh;
-        width: 100%;
-
-        z-index: 9000;
-    `)
-
-    return (
-        <Fragment/>
-    )
+    return (<Fragment/>)
 })
 
 class App extends React.Component {
