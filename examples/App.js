6125846c80de1fbe13693d5fb0ca15d4d85d3297
bhbh1226 <bhbh1226@naver.com>
2019-04-03 21:56:17 +0900

    Revert "Revert "custom Style add""
    This reverts commit 6e2b4b233ae4532d1224521ae8909e2db72102ab.

--- examples/App.js
+++ examples/App.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import ModalSupervisor, { modalRootInit } from '../src';
+import { STYLE_BACKGROUND, StyleManagerHOC } from '../src/components/StyleManager'
+import styled from 'styled-components'
 
 import './normalize.css'
-// import Header from './components/Header'
 
 modalRootInit()
+
+const NewCustomStyle = StyleManagerHOC(({styleOverride}) => {
+    styleOverride(STYLE_BACKGROUND, styled.div`
+        background-color: blue;
+        position: fixed;
+
+        top: 0;
+        left: 0;
+
+        height: 100vh;
+        width: 100%;
+
+        z-index: 9000;
+    `)
+
+    return (
+        <Fragment/>
+    )
+})
 
 class App extends React.Component {
     render() {
         return (
             <div id="app">
+                <NewCustomStyle/>
                 <ModalSupervisor>
                     {this.props.children}
                 </ModalSupervisor>
