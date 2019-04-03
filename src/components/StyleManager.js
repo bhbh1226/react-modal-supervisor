fabbed343bfe2c27829e568dafb8a4940332bb2b
bhbh1226 <bhbh1226@naver.com>
2019-04-03 22:22:35 +0900

    button customizing add

--- src/components/StyleManager.js
+++ src/components/StyleManager.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { ConfirmButton as CBTN, DismissButton as DBTN, CloseButtonAbsolute as CBTNA } from './Button'
 import { ModalBackground as MB, ModalInner as MI, ModalActionContainer as MAC, PromptInputText as PIT } from './Container'
 
 let ModalBackground = MB
 let ModalInner = MI
 let ModalActionContainer = MAC
 let PromptInputText = PIT
+// let Button = BTN
+let ConfirmButton = CBTN
+let DismissButton = DBTN
+let CloseButtonAbsolute = CBTNA
 
 /* Constants */
 const STYLE_BACKGROUND = 200
 const STYLE_INNER = 201
 const STYLE_ACTION_CONTAINER = 202
 const STYLE_INPUT_TEXT = 203
+const STYLE_CONFIRM_BUTTON = 204
+const STYLE_DISMISS_BUTTON = 205
+const STYLE_CLOSE_BUTTON_ABSOLUTE = 206
 
 function styleOverride(style, component) {
     switch(style) {
@@ -27,6 +35,15 @@ function styleOverride(style, component) {
         case STYLE_INPUT_TEXT:
             PromptInputText = component
             break;
+        case STYLE_CONFIRM_BUTTON:
+            ConfirmButton = component
+            break;
+        case STYLE_DISMISS_BUTTON:
+            DismissButton = component
+            break;
+        case STYLE_CLOSE_BUTTON_ABSOLUTE:
+            CloseButtonAbsolute = component
+            break;
     }
 }
 
@@ -39,5 +56,6 @@ const StyleManagerHOC = (WrappedComponent) => (props) => {
 export { 
     STYLE_BACKGROUND, STYLE_INNER, STYLE_ACTION_CONTAINER, STYLE_INPUT_TEXT,
     ModalBackground, ModalInner, ModalActionContainer, PromptInputText, 
+    ConfirmButton, DismissButton, CloseButtonAbsolute,
     StyleManagerHOC }
 // export default StyleManager
